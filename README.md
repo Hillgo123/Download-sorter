447d9e8e3cef0df47092cda3716778a8950957b6
Leo Bergqvist <leo@bergqvist.se>
2022-09-03 22:11:52 +0200

    Refactoring and accessabilty
    Refactored code.
    Added accessibility options, i.e easier to change the directory where it takes files from and creates folders for where you place the files.
    Added documentation.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Download-sorter
- This is a python sorter that takes the files from your downloads and sorts them into folders based on the file type
+ This is a python sorter that takes the files from your downloads and sorts them into folders based on the file type.
--- index.py
+++ index.py
@@ -1,70 +1,80 @@
-import sched, os, time, shutil
+import os, shutil, sched, time
 
 
 
 class file_handler():
+    def __init__(self, dir):
+        self.dir = dir
+        self.schedule = sched.scheduler(time.time, time.sleep)
 
-    def move(self):
+    #Creating files to sort into
+    def create(self, dir_name):
+        if not os.makedirs(dir_name, exist_ok = True): #Check if directory already exist
+            os.makedirs(dir_name, exist_ok = True)
+
+    def move(self, type, destination):
         #Moving files
-        for filename in os.listdir(r"C:\Users\leo.bergqvist1\Downloads"):
+        for file_name in os.listdir(self.dir):
             #Where you move files from
-            src = r"C:\Users\leo.bergqvist1\Downloads/" + filename
+            src = self.dir + file_name
 
             #Sorter for different file types
-
-            if filename.lower().endswith("txt"):
-                moved = r"C:\Downloads\text/" + filename
+            if file_name.lower().endswith(type):
+                moved = destination + file_name
                 shutil.move(src, moved)
                 print(moved)
 
-            elif filename.lower().endswith("docx"):
-                moved = r"C:\Downloads\word/" + filename
-                shutil.move(src, moved)
-                print(moved)
+    #Moving filetypes not mentiond into a seperate "other" folder
+    def other(self, destination):
 
-            elif filename.lower().endswith("exe"):
-                moved = r"C:\Downloads\installers/" + filename
-                shutil.move(src, moved)
-                print(moved)
-
-            elif filename.lower().endswith("pdf"):
-                moved = r"C:\Downloads\pdf/" + filename
-                shutil.move(src, moved)
-                print(moved)
-
-            elif filename.lower().endswith("zip"):
-                moved = r"C:\Downloads\zip/" + filename
-                shutil.move(src, moved)
-                print(moved)
-
-            elif filename.lower().endswith("cvc"):
-                moved = r"C:\Downloads\cvc/" + filename
-                shutil.move(src, moved)
-                print(moved)
-
-            elif filename.lower().endswith("png") or filename.lower().endswith("jpg") or filename.lower().endswith("tiff"):
-                moved = r"C:\Downloads\images/" + filename
-                shutil.move(src, moved)
-                print(moved)
-
-            else:
-                moved = r"C:\Downloads\other/" + filename
-                shutil.move(src, moved)
-                print(moved)
-            
-
-            
-
-            
+        for file_name in os.listdir(self.dir):
+            src = self.dir + file_name
+            moved = destination + file_name
+            shutil.move(src, moved)
+            print(moved)
 
 
-File_Handler = file_handler()
+    def update(self):
+        #Creating directories
+        File_Handler.create(r"C:\Downloads\text/")
+        File_Handler.create(r"C:\Downloads\word/")
+        File_Handler.create(r"C:\Downloads\installers/")
+        File_Handler.create(r"C:\Downloads\pdf/")
+        File_Handler.create(r"C:\Downloads\zip/")
+        File_Handler.create(r"C:\Downloads\images/")
+        File_Handler.create(r"C:\Downloads\other/")
+
+
+
+        #Activating the move function for different file types
+        File_Handler.move(".txt", r"C:\Downloads\text/")
+        File_Handler.move(".docx", r"C:\Downloads\word/")
+        File_Handler.move(".exe", r"C:\Downloads\installers/")
+        File_Handler.move(".pdf", r"C:\Downloads\pdf/")
+        File_Handler.move(".zip", r"C:\Downloads\zip/")
+        File_Handler.move(".cvc", r"C:\Downloads\cvc/")
+        File_Handler.move(".png", r"C:\Downloads\images/")
+        File_Handler.move(".jpg", r"C:\Downloads\images/")
+        File_Handler.other(r"C:\Downloads\other/")
+
+        self.schedule.enter(10, 1, File_Handler.update())
+
+        
+
+    def run(self):
+        #Run code in the background to continually update
+        self.schedule.enter(10, 1, File_Handler.update())
+        self.schedule.run()
+
+                      
+
+        
+File_Handler = file_handler(r"C:\Users\leo.bergqvist1\Downloads/") #selecting where to take files from
 
 def main():
-    File_Handler.move()
+    File_Handler.run()
             
 
-
+#Run code
 if __name__ == "__main__":
     main()
-
