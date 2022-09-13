d151d6f2e59a6e04e92c99e6b88078dac2e9b48f
Leo Bergqvist <leo@bergqvist.se>
2022-09-13 11:11:04 +0200

    Excel file + automatic
    Added an excel file that stores information on the moved files.
    Added information in README.md to run the script automatically.

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
  This is a python sorter that takes the files from your downloads and sorts them into folders based on the file type.
 
  In order to add more file types add 
- File_Handler.create(r"C:\Downloads\{folder name}/")
+ r"C:\Downloads\{folder name}/" to the self.directories list (line 6)
  and
  File_Handler.move("{file type}", r"C:\Downloads\{file name}/")
- to the update function within the file handler class.
+ to the update function within the file handler class. (line 49)
+
+ In order to run automatically https://www.youtube.com/watch?v=OjykudimEqc (on windows)
+
+ It stores data on the files moved (File Name, File Type and File Size) in the tracker.xlsx file
--- index.py
+++ index.py
@@ -1,8 +1,21 @@
-import os, shutil
+import os, shutil, time
+from datetime import date
+import pandas as pd
 
 class file_handler():
     def __init__(self, dir):
         self.dir = dir
+        self.directories = [
+            r"C:\Users\leo.bergqvist1\Downloads\pdf/",
+            r"C:\Users\leo.bergqvist1\Downloads\word/",
+            r"C:\Users\leo.bergqvist1\Downloads\installers/",
+            r"C:\Users\leo.bergqvist1\Downloads\pdf/",
+            r"C:\Users\leo.bergqvist1\Downloads\zip/",
+            r"C:\Users\leo.bergqvist1\Downloads\images/",
+            r"C:\Users\leo.bergqvist1\Downloads\cvc/",
+            r"C:\Users\leo.bergqvist1\Downloads\other/"
+        ]
+        self.excel = {"File Name": [], "File Type": [], "File Size (bytes)": []} #Creates dictionary to keep track of downloaded files
 
     #Creating files to sort into
     def create(self, dir_name):
@@ -16,39 +29,53 @@ class file_handler():
             src = self.dir + file_name
 
             #Sorter for different file types
-            if file_name.lower().endswith(type):
-                moved = destination + file_name
-                shutil.move(src, moved)
-                print(moved)
+            if not os.path.isdir(self.dir + file_name): #Makes sure it dosen't move the directories for downloaded files
+                if file_name.lower().endswith(type):
+                    moved = destination + file_name
+
+                    #Appends information about the moved file to the dictionary
+                    self.excel["File Name"].append(str(file_name))
+                    self.excel["File Type"].append(str(type))
+                    self.excel["File Size (bytes)"].append(str(os.stat(src).st_size))
+
+                    shutil.move(src, moved) #Moves file
+                    print(moved)
+
+    #Converts the dictionary filled with file information to an excel file
+    def to_excel(self):
+        df = pd.DataFrame(self.excel)
+
+        df.to_excel("tracker.xlsx", sheet_name = date.today().strftime("%B %d, %Y"))
 
     #Moving filetypes not mentiond into a seperate "other" folder
     def other(self, destination):
         for file_name in os.listdir(self.dir):
-            src = self.dir + file_name
-            moved = destination + file_name
-            shutil.move(src, moved)
-            print(moved)
+            if not os.path.isdir(self.dir + file_name): #Makes sure it dosen't move the directories for downloaded files
+                src = self.dir + file_name
+                moved = destination + file_name
+                shutil.move(src, moved)
+                print(moved)
 
     def update(self):
         #Creating directories
-        File_Handler.create(r"C:\Downloads\text/")
-        File_Handler.create(r"C:\Downloads\word/")
-        File_Handler.create(r"C:\Downloads\installers/")
-        File_Handler.create(r"C:\Downloads\pdf/")
-        File_Handler.create(r"C:\Downloads\zip/")
-        File_Handler.create(r"C:\Downloads\images/")
-        File_Handler.create(r"C:\Downloads\other/")
+        for dir in self.directories:
+            File_Handler.create(dir)
 
         #Activating the move function for different file types
-        File_Handler.move(".txt", r"C:\Downloads\text/")
-        File_Handler.move(".docx", r"C:\Downloads\word/")
-        File_Handler.move(".exe", r"C:\Downloads\installers/")
-        File_Handler.move(".pdf", r"C:\Downloads\pdf/")
-        File_Handler.move(".zip", r"C:\Downloads\zip/")
-        File_Handler.move(".cvc", r"C:\Downloads\cvc/")
-        File_Handler.move(".png", r"C:\Downloads\images/")
-        File_Handler.move(".jpg", r"C:\Downloads\images/")
-        File_Handler.other(r"C:\Downloads\other/")
+        File_Handler.move(".docx", r"C:\Users\leo.bergqvist1\Downloads\word/")    
+        File_Handler.move(".exe", r"C:\Users\leo.bergqvist1\Downloads\installers/")
+        File_Handler.move(".pdf", r"C:\Users\leo.bergqvist1\Downloads\pdf/")
+        File_Handler.move(".zip", r"C:\Users\leo.bergqvist1\Downloads\zip/")
+        File_Handler.move(".cvc", r"C:\Users\leo.bergqvist1\Downloads\cvc/")
+        File_Handler.move(".png", r"C:\Users\leo.bergqvist1\Downloads\images/")
+        File_Handler.move(".jpg", r"C:\Users\leo.bergqvist1\Downloads\images/")
+        File_Handler.other(r"C:\Users\leo.bergqvist1\Downloads\other/")
+
+        print("Files successfully moved!")
+        time.sleep(10)
+        File_Handler.to_excel()
+        
+
 
 #Defining the file handler        
 File_Handler = file_handler(r"C:\Users\leo.bergqvist1\Downloads/") #selecting where to take files from
